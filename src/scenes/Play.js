f3367810c4e5ea2ebaf6a1d54cbf715234d741c6
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2023-04-25 16:08:52 -0700

    fixing background

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -16,7 +16,7 @@ class Play extends Phaser.Scene{
 
     create(){
         //place tile sprite
-        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'improved_starfield').setOrigin(0, 0);
+        this.improved_starfield = this.add.tileSprite(0, 0, 640, 480, 'improved_starfield').setOrigin(0, 0);
 
         //green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
