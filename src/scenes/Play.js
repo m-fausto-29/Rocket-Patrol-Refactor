413df2f3a84de0ff696bc3b8292861179dcd1477
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2023-04-25 16:04:04 -0700

    Updating starfield

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,7 +8,7 @@ class Play extends Phaser.Scene{
         //load images/tile sprites
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
-        this.load.image('starfield', './assets/starfield.png');
+        this.load.image('improved_starfield', './assets/improved_starfield.png');
 
         // load spritesheet
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
@@ -16,7 +16,7 @@ class Play extends Phaser.Scene{
 
     create(){
         //place tile sprite
-        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'improved_starfield').setOrigin(0, 0);
 
         //green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
@@ -88,7 +88,7 @@ class Play extends Phaser.Scene{
             this.scene.start("menuScene");
         }
 
-        this.starfield.tilePositionX -= 4;
+        this.improved_starfield.tilePositionX -= 4;
 
         if(!this.gameOver) {
             this.p1Rocket.update();             // update rocket sprite
