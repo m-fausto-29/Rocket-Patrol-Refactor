67738fc9ce4db54ae538a7a88ee6a54a61637974
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2023-05-01 12:11:10 -0700

    Adding final comments

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
 // main file
+/*
+Name: Monserrat Fausto
+Mod Title: Rocket Patrol: Return of the Rocket
+Estimated Hours: 11.5 hours
+List of Mods: 
+ - Crashing into non-ship object lose points (10): Approved by TA, I created a separate class for this object and named it 'Asteroid'
+    the idea being that the player would lose points if they hit this type of object and must try to avoid hitting it to avoid this outcome
+ - Create a new scrolling tile sprite for the background (5)
+ - Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (15)
+ - Implement the speed increase that happens after 30 seconds in the original game (5)
+ - Display the time remaining (in seconds) on the screen (10)
+ - Implement mouse control for player movement and mouse click to fire (15)
+ - Add your own (copyright-free) background music to the Play scene (please be mindful of the volume) (5)
+ - Create 4 new explosion sound effects and randomize which one plays on impact (10)
+ - Create a new title screen (e.g., new artwork, typography, layout) (10)
+ - Implement the 'FIRE' UI text from the original game (5)
+Citations:
+ - freesound.org: Used to find copyright-free sfx and background music
+ - pixilart.com: Used to create custom sprites, title screen, and new scrolling tile sprite for background
+ - Used as reference for random sfx: https://stackoverflow.com/questions/39604400/play-a-random-sound-on-single-button
+ - Used as reference for mouse movement and clicking: https://steemit.com/utopian-io/@onepice/move-objects-according-to-the-mouse-position-with-phaser-3
+ - Used as reference for time display: https://stackoverflow.com/questions/63870145/phaser-3-create-a-game-clock-with-minutes-and-seconds
+*/
 let config = {
     type: Phaser.AUTO,
     width: 640,
